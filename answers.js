c840ee1e3027c1e5902268dd3621f0f514c2836c
Sergio Souza <emailsouza@gmail.com>
2018-06-18 17:14:47 -0600

    3rd commit - performed operations on the crazy object

--- answers.js
+++ answers.js
@@ -45,3 +45,48 @@ const superMario = {
 // Write code that will access your hobby from the object that you just created.
 console.log(superMario.datatype);
 
+const crazyObject = {
+  taco: [{
+    meat: 'steak',
+    cheese: ['panela', 'queso', 'chihuahua']
+  },
+  {
+    meat: 'chicken',
+    salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+  },
+  ],
+  larry: {
+    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+    characters: [{
+      name: "Jeff",
+      occupation: "manager"
+    },
+    {
+      name: "funkhauser",
+      occupation: "tv dude"
+    },
+    {
+      name: "susie",
+      occupation: "jeffs wife",
+      favourtieHobby: "Swearing at Larry and Jeff"
+    },
+    ]
+  }
+}
+
+// Use crazy Object to log the following.
+// "omg my mouth is burning"
+console.log(crazyObject.taco[1].salsa[5]);
+
+// "Pretty pretty prettayyyyy good"
+console.log(crazyObject.larry.quotes[0]);
+
+// "Swearing at Larry and Jeff"
+console.log(crazyObject.larry.characters[2].favourtieHobby);
+
+// "Chicken Teriyaki Boyyyyyy"
+console.log(crazyObject.larry.nicknames[1]);
+
+// The object the contains the name funkhauser
+console.log(crazyObject.larry.characters[1].name);
